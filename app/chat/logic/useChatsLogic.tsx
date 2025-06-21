75edbe41ebce1ce727894b745da46f6b428d9f26
Hamza Hassan <hassanhamza930@gmail.com>
2025-06-22 04:46:24 +0500

    Ability to Create New Chats by Default

--- app/chat/logic/useChatsLogic.tsx
+++ app/chat/logic/useChatsLogic.tsx
@@ -1,21 +1,42 @@
-import { useEffect } from "react";
+import { useEffect, useState } from "react";
 import { useChatStore } from "../store/store";
 import { CHATS_STORAGE_KEY, readChatsFromLocalStorage } from "./chatUtils";
 
 export const useChatsLogic = () => {
-    const { chats, setChats } = useChatStore();
+    const { chats, setChats, setSelectedChat, addChat, selectedChat } = useChatStore();
+    const [initialized, setInitialized] = useState(false);
 
-    // Load chats from local storage
+    //load chats from local storage and set them to the store
     useEffect(() => {
-        var chats = readChatsFromLocalStorage();
-        setChats({ chats });
+        const loadedChats = readChatsFromLocalStorage();
+        setChats({ chats: loadedChats });
+
+        if (!selectedChat) {
+            if (loadedChats.length > 0) {
+                const latestChat = [...loadedChats].sort((a, b) =>new Date(b.timeUpdated).getTime() - new Date(a.timeUpdated).getTime())[0];
+                setSelectedChat({ chat: latestChat });
+            } else {
+                addChat({
+                    chat: {
+                        id: crypto.randomUUID(),
+                        name: "New Chat",
+                        messages: [],
+                        timeUpdated: new Date(),
+                    },
+                });
+            }
+        }
+        setInitialized(true);
+        // eslint-disable-next-line react-hooks/exhaustive-deps
     }, []);
 
 
     //store chats in local storage everytime the chats state changes
     useEffect(() => {
-        localStorage.setItem(CHATS_STORAGE_KEY, JSON.stringify(chats));
-    }, [chats]);
+        if (initialized) {
+            localStorage.setItem(CHATS_STORAGE_KEY, JSON.stringify(chats));
+        }
+    }, [chats, initialized]);
 
     return { chats, setChats };
 };
